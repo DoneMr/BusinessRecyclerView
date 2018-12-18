17ec775ee92c455e1d43b47fe4544d60409b0314
maruilong <591462081@qq.com>
2018-12-19 00:36:01 +0800

    增加jcenter配置

--- bizrecyclerviewlib/src/main/java/com/done/bizrecyclerviewlib/features/BizPagerSnapHelper.java
+++ bizrecyclerviewlib/src/main/java/com/done/bizrecyclerviewlib/features/BizPagerSnapHelper.java
@@ -11,6 +11,9 @@ import android.support.v7.widget.RecyclerView.SmoothScroller.ScrollVectorProvide
 import android.util.DisplayMetrics;
 import android.view.View;
 
+import com.done.bizrecyclerviewlib.adpater.BizBaseAdapter;
+import com.done.bizrecyclerviewlib.cell.IBizCell;
+
 /**
  * File: com.done.bizrecyclerviewlib.features.BizPagerSnapHelper.java
  * Description: xxx
@@ -26,6 +29,16 @@ public class BizPagerSnapHelper extends BaseBizSnapHelper {
     private OrientationHelper mVerticalHelper;
     @Nullable
     private OrientationHelper mHorizontalHelper;
+
+    /**
+     * 是否是自行处理产生的滚动
+     */
+    private boolean isSnap = false;
+
+    /**
+     * 当前展示的item
+     */
+    private View mTargetView = null;
 
     public BizPagerSnapHelper() {
     }
@@ -45,7 +58,8 @@ public class BizPagerSnapHelper extends BaseBizSnapHelper {
         } else {
             out[1] = 0;
         }
-
+        isSnap = true;
+        mTargetView = targetView;
         return out;
     }
 
@@ -67,9 +81,9 @@ public class BizPagerSnapHelper extends BaseBizSnapHelper {
         } else {
             View mStartMostChildView = null;
             if (layoutManager.canScrollVertically()) {
-                mStartMostChildView = this.findStartView(layoutManager, this.getVerticalHelper(layoutManager));
+                mStartMostChildView = this.findStartView(layoutManager, getVerticalHelper(layoutManager));
             } else if (layoutManager.canScrollHorizontally()) {
-                mStartMostChildView = this.findStartView(layoutManager, this.getHorizontalHelper(layoutManager));
+                mStartMostChildView = this.findStartView(layoutManager, getHorizontalHelper(layoutManager));
             }
 
             if (mStartMostChildView == null) {
@@ -123,8 +137,10 @@ public class BizPagerSnapHelper extends BaseBizSnapHelper {
 
             @Override
             protected int calculateTimeForScrolling(int dx) {
-                return Math.min(100, super.calculateTimeForScrolling(dx));
+                return Math.min(MAX_SCROLL_ON_FLING_DURATION, super.calculateTimeForScrolling(dx));
             }
+
+
         };
     }
 
@@ -208,6 +224,14 @@ public class BizPagerSnapHelper extends BaseBizSnapHelper {
         }
 
         return this.mHorizontalHelper;
+    }
+
+    @Override
+    public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+    }
+
+    @Override
+    public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
     }
 
     public interface OnPageChangeListener {
